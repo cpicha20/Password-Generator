df26f4530c961405631f437339f2bc24c571c969
cpicha20 <cpichardowps@gmail.com>
2023-09-28 20:23:10 -0400

    added confirms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
 let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
-"P","Q","R","S","T","U","V","W","X","Y","Z"]
-let numbersArr = ['1','2','3','4','5','6','7','8','9','0']
-let specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-let userPW =[]
+"P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numbersArr = ['1','2','3','4','5','6','7','8','9','0'];
+let specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let userPW = [];
 
 function generatePassword() {
   var length= prompt("How many characters long would you like your password to be? (Please pick a number between 8-128)");
-  if(length>8||length<128)
+  if(length>=8 && length<=128)
   {
-    if (confirm("Thank you, Would you like")){
-      userPW.concat(upperCase);
+    
+    if(confirm("uppercase?")){
+      userPW=userPW.concat(upperCase);
+      console.log(userPW);
     }
-    if (confirm("Thank you, Would you like")){
-      userPW.concat(lowerCase);
+    if(confirm("lower?")){
+      userPW=userPW.concat(lowerCase);
+      console.log(userPW);
     }
-    if (confirm("Thank you, Would you like")){
-      userPW.concat(numbersArr);
+    if(confirm("numbers?")){
+      userPW=userPW.concat(numbersArr);
+      console.log(userPW);
     }
-    if (confirm("Thank you, Would you like")){
-      userPW.concat(specialArr);
+    if(confirm("special?")){
+      userPW=userPW.concat(specialArr);
+      console.log(userPW);
     }
+    console.log(userPW);
+
     
   }
   else{ 
