65a1517fae6b16b1dc5efac21de50c918e97381c
cpicha20 <cpichardowps@gmail.com>
2023-09-28 21:04:41 -0400

    added random selection property

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,59 @@
+
 let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 "P","Q","R","S","T","U","V","W","X","Y","Z"];
 let numbersArr = ['1','2','3','4','5','6','7','8','9','0'];
 let specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-let userPW = [];
 
+
+
+//
 function generatePassword() {
-  var length= prompt("How many characters long would you like your password to be? (Please pick a number between 8-128)");
-  if(length>=8 && length<=128)
+  var arrLength = prompt("How many characters long would you like your password to be? (Please pick a number between 8-128)");
+  var tempPW = [];
+  var userPW = [];
+  if(arrLength>=8 && arrLength<=128)
   {
     
     if(confirm("uppercase?")){
-      userPW=userPW.concat(upperCase);
-      console.log(userPW);
+      tempPW=tempPW.concat(upperCase);
+      console.log(tempPW);
     }
-    if(confirm("lower?")){
-      userPW=userPW.concat(lowerCase);
-      console.log(userPW);
-    }
-    if(confirm("numbers?")){
-      userPW=userPW.concat(numbersArr);
-      console.log(userPW);
-    }
-    if(confirm("special?")){
-      userPW=userPW.concat(specialArr);
-      console.log(userPW);
-    }
-    console.log(userPW);
 
+    if(confirm("lower?")){
+      tempPW=tempPW.concat(lowerCase);
+      console.log(tempPW);
+    }
+
+    if(confirm("numbers?")){
+      tempPW=tempPW.concat(numbersArr);
+      console.log(tempPW);
+    }
+
+    if(confirm("special?")){
+      tempPW=tempPW.concat(specialArr);
+      console.log(tempPW);
+    }
+    
+    console.log(tempPW);
+    
+    for (let index = 0; index < arrLength; index++) {
+    
+      rndPick = tempPW[Math.floor(Math.random() * tempPW.length)];
+      userPW.push(rndPick);      
+    }
+  userPW=userPW.join('');
+  console.log(userPW);
+  return userPW;
     
   }
   else{ 
     alert("Incorrect input. Please Pick a number between 8-128");
+    return"";
   } 
   
 }
-
-
-
 
 
 // Assignment Code
